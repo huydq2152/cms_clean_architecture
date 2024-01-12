d0b6a9ea045af5220b7a447af9f67c383c20b811
huy <huy.dq1@bimgroup.com>
2024-01-12 15:01:41 +0700

    update admin-ui

--- admin-ui/src/app/views/content/content-routing.module.ts
+++ admin-ui/src/app/views/content/content-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostComponent } from './post/post.component';
+import { PostCategoryComponent } from './post-category/post-category.component';
 
 const routes: Routes = [
   {
@@ -13,6 +14,13 @@ const routes: Routes = [
     component: PostComponent,
     data: {
       title: 'posts',
+    },
+  },
+  {
+    path: 'post-categories',
+    component: PostCategoryComponent,
+    data: {
+      title: 'post categories',
     },
   },
 ];
