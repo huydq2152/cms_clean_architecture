1512becc78e1d0d37db9a8acce69347a5f171544
huy <huy.dq1@bimgroup.com>
2024-02-06 11:05:38 +0700

    add DictionaryExtension

---
+++ src/BuildingBlocks/Shared/Extensions/DictionaryExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.Extensions
+{
+    public static class DictionaryExtension
+    {
+        public static void Merge<TK, TV>(this IDictionary<TK, TV> target, IDictionary<TK, TV> source, bool overwrite = false)
+        {
+            source.ToList().ForEach(_ => {
+                if ((!target.ContainsKey(_.Key)) || overwrite)
+                    target[_.Key] = _.Value;
+            });
+        }
+    }
+}
