584523b0d1535aab4de63b931d9adacda836459e
huydq2152 <quanghuy2152@gmail.com>
2024-02-23 08:50:50 +0700

    use nswag to generate api + add primeng and use alert when login false : for admin UI project

--- admin-ui/src/app/views/auth/login/login.component.ts
+++ admin-ui/src/app/views/auth/login/login.component.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import {
+  AdminApiAuthApiClient,
+  AuthenticatedResult,
+  LoginRequest,
+} from 'src/app/api/admin-api.service.generated';
+import { AlertService } from 'src/app/shared/services/alert.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  loginFrom: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private authApiClient: AdminApiAuthApiClient,
+    private alertService: AlertService,
+    private router: Router
+  ) {
+    this.loginFrom = this.fb.group({
+      userName: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
 
+  login() {
+    var request: LoginRequest = new LoginRequest({
+      userName: this.loginFrom.value.userName,
+      password: this.loginFrom.value.password,
+    });
+    this.authApiClient.login(request).subscribe({
+      next: (res: AuthenticatedResult) => {
+        // Save token to local storage
+
+        // Redirect to dashboard
+        this.router.navigate(['/dashboard']);
+      },
+      error: (error) => {
+        this.alertService.showError('Login failed!');
+      },
+    });
+  }
 }
